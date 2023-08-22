a48c0064feb23f3dd4734b46cda3a93861ab20e2
Ivan Glazunov <ivan.glaz.2029@mail.ru>
2023-08-22 14:53:45 +0300

    Extenstions string

---
+++ back/UniversistetAPI/Extessions/StringExtenssions.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversistetAPI.Extessions
+{
+    public static class StringExtenssions
+    {
+        public static string ToUpperFirstChar(this string str)
+        {
+            var chars = str.ToCharArray();
+            var result = char.ToUpper(chars[0]) + str.Substring(1);
+            return result.ToString();
+        }
+    }
+}
