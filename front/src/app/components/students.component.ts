9f7b5768a4e4edb89911f9ad4d0b42fbfc365f36
Ivan <ivan.glaz.2029@mail.ru>
2023-08-29 13:37:09 +0300

    ангуляр.студенты

---
+++ front/src/app/components/students.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-view-students',
+    templateUrl: './students.component.html'
+    ,styleUrls: ['./students.component.css']
+})
+
+export class StudentsViewsComponent implements OnInit{
+    constructor(){
+
+    }
+    ngOnInit(): void{
+
+    }
+}
